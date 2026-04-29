3d97bbd5c2f185cc4b7607cd7b54a1e68233f8c3
Zgarthy <121491349+Zgarthy@users.noreply.github.com>
2026-04-29 19:31:52 +0100

    Update html_actuator.js

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -70,6 +70,8 @@ HTMLActuator.prototype.addTile = function (tile) {
   inner.textContent = tile.value;
   if (tile.value === 40) inner.textContent = "0";
   if (tile.value === 37) inner.textContent = "🧊";
+  if (tile.value === 38) inner.textContent = "🧊";
+  if (tile.value === 39) inner.textContent = "🧊";
 
   if (tile.previousPosition) {
     // Make sure that the tile gets rendered in the previous position first
